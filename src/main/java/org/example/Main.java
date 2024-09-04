23c8435c98c2cb4226b62187fb62a6f0f2f93405
Fabio <Fabio@ada-school.com>
2024-09-04 00:03:31 -0300

    Add some imports

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,4 +1,6 @@
 package org.example;
+import org.example.strategy.BlackFridayDiscountStrategy;
+import org.example.strategy.SeasonalDiscountStrategy;
 
 public class Main {
     public static void main(String[] args) {
